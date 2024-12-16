8d631e72c72086d52d6a6bd489bffd46835e0fff
Masud-Rahman22 <masudrhmn6560@gmail.com>
2024-12-17 01:54:24 +0600

    logged in user with informations in dashboard after github log in

--- src/app/(withDashboard)/dashboard/page.tsx
+++ src/app/(withDashboard)/dashboard/page.tsx
@@ -1,7 +1,19 @@
-const DashboardPage = () => {
+import { authOptions } from "@/utils/authOptions";
+import { getServerSession } from "next-auth";
+import Image from "next/image";
+
+const DashboardPage = async() => {
+  const session = await getServerSession(authOptions)
+ console.log(session)
   return (
     <div>
-      <h1 className="text-4xl text-center mt-10">Welcome To Dashboard Page</h1>
+     { session?.user &&
+      <>
+       <h1 className="text-4xl text-center mt-10">Welcome To {session?.user?.name}</h1>
+      <h1 className="text-4xl text-center mt-10">User Email : {session?.user?.email}</h1>
+      <Image className="mx-auto mt-5 rounded-full" src={session?.user?.image || "https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.25&w=256&h=256&q=80"} alt="user image" height={200} width={200}/>
+      </>
+     }
     </div>
   );
 };
